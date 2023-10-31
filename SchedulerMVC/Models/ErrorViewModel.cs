aecbc086981ebb93b005db0c5731585eac8337d3
Andrew Corrigan <andrew.corrigan.98@gmail.com>
2023-10-31 21:21:15 +0000

    Switched to MVC with placeholder DB

---
+++ SchedulerMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SchedulerMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
